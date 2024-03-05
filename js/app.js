3ab4fb2b38e33d580da78aa04ae655f8cfbdb1a4
Shrihari Magar <shriharimagar1@gmail.com>
2024-03-05 16:37:33 +0530

    fix: anime js resolved

--- js/app.js
+++ js/app.js
@@ -314,3 +314,15 @@ function handleDropdowns() {
     });
   }
 }
+
+// anime({
+//   targets: "div.heading",
+//   translateY: [
+//     { value: 200, duration: 500 },
+//     { value: 0, duration: 800 },
+//   ],
+//   delay: function (el, i, l) {
+//     return i * 1000;
+//   },
+//   loop: true,
+// });
