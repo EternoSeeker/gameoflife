6c149a90171709732f24735890356000e53ab0c2
Partha Das <138643473+partha120804@users.noreply.github.com>
2024-07-14 22:01:44 +0530

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,11 +1,15 @@
-const bgMusic = document.getElementById('bg-music');
+  const bgMusic = document.getElementById('bg-music');
+    let musicOn = true;
 
-function toggleMusic() {
-  if (bgMusic.paused) {
-    bgMusic.play();
-    document.querySelector('.toggle-button').textContent = 'Music Off'; // Change button text
-  } else {
-    bgMusic.pause();
-    document.querySelector('.toggle-button').textContent = 'Music On'; // Change button text
-  }
-}
+    function toggleMusic() {
+      if (musicOn) {
+        bgMusic.pause();
+        document.querySelector('.music-text').textContent = 'Music Off'; // Change button text
+        document.querySelector('.toggle-button i').className = 'fa-solid fa-volume-xmark';
+      } else {
+        bgMusic.play();
+        document.querySelector('.music-text').textContent = 'Music On'; // Change button text
+        document.querySelector('.toggle-button i').className = 'fa-solid fa-volume-high'; 
+      }
+      musicOn = !musicOn; 
+    }
