a344398771cb6cef96e3f82e21f39562be7d1f81
Jasleen1210 <05kaur.j@gmail.com>
2024-07-17 21:08:49 +0530

    Edit for smaller screens
    >closing sidenav on load because it conflicts with intro boxes
    > skipping popups that point to sidenav when screens are smaller because itll be closed
    > added symbols instead of buttons in smaller screens as buttons occupy more space
    > working on getting more accurate positioning for the popups using js, need more time to execute that

--- js/introduction.js
+++ js/introduction.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    closeNavDeafult();//when screen is small, auto close the sidenav on load because otherwise it hinders the introseries 
     startLoader();  
     if (!sessionStorage.getItem('introCompleted')) {
         showBox(1);
@@ -9,7 +10,16 @@ function showBox(step) {
     document.querySelectorAll('.intro-page').forEach(stepDiv => {
         stepDiv.style.display='none';
     });
-    document.getElementById(`page${step}`).style.display= 'block';
+    if(isSmall){
+        if(step==6 || step==7){
+         showBox(8); //if the screen size is small, the sidenav will be closed and we need to skip the settings wale intro popups
+        }
+        else{
+            document.getElementById(`page${step}`).style.display= 'block';
+        }}
+    else{
+        document.getElementById(`page${step}`).style.display= 'block';
+    }
 }
 
 function closeIntroSeries(){
