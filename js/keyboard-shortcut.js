f19158da1bb8ffdee7591841e4b1e516bc365367
VijaySamant4368 <vijaysamant4368@gmail.com>
2024-07-29 00:58:41 +0530

    Mapped history to keys

--- js/keyboard-shortcut.js
+++ js/keyboard-shortcut.js
@@ -19,7 +19,12 @@ document.addEventListener("keydown", function (event) {
       ARROWDOWN: () => updateRandomValue(-5),
       G: toggleGrid,
       M: toggleMusic,
-      W: toggleWarp
+      W: toggleWarp,
+      1: () => drawHistory(1),
+      2: () => drawHistory(2),
+      3: () => drawHistory(3),
+      4: () => drawHistory(4),
+      5: () => drawHistory(5)
     };
   
     // Check if a function is mapped to the pressed key
@@ -28,3 +33,20 @@ document.addEventListener("keydown", function (event) {
       keyMap[keyPressed]();
     }
   });
+
+  
+function drawHistory(key) {
+  const index = key - 1
+  patterns = getPatternHistory(); //Get the array of all the patterns
+  if (patterns.length < index) {
+    console.log(key, "key pressed. There are ", patterns.length," histories yet.")
+    return
+  }
+  pattern = patterns[index]
+  // Set the cells array to the corresponding pattern
+  if (!isAnimating && !isStarted) {
+    cells = pattern;
+    aliveCount = (cells.flat().filter(cell => cell === ALIVE).length);
+  }
+  drawCells();
+}
